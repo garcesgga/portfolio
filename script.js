f6a387ed3db1e5f23a986de2744e68b77c42fafd
garces <guilherme.almada@uvvnet.com.br>
2025-01-03 21:19:41 -0300

    Uploading my professional website
    This repository contains an index, style and script, as well as images.

---
+++ script.js
@@ -0,0 +1,14 @@
+// Função para habilitar o scroll suave
+function initSmoothScrolling() {
+    const navLinks = document.querySelectorAll('nav a');
+    navLinks.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            const targetId = this.getAttribute('href');
+            const targetElement = document.querySelector(targetId);
+            targetElement.scrollIntoView({ behavior: 'smooth' });
+        });
+    });
+}
+
+initSmoothScrolling();
